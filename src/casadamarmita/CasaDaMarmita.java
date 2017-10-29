c5384cfe2ab703e6b9808364bcdd837d374f2fc0
Bruno Caixeta <brunoalvescaixeta@hotmail.com>
2017-10-29 15:36:08 -0200

    Teste Inicializando variavel

--- src/casadamarmita/CasaDaMarmita.java
+++ src/casadamarmita/CasaDaMarmita.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package casadamarmita;
 
-/**
- *
- * @author Luiz
- */
+
 public class CasaDaMarmita {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pedido pedido = new Pedido();
     }
     
 }
