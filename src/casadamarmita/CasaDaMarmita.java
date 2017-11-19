731640455bd5928292a6a8f4c8c5114fc3dd60e9
Luiz Henrique Gonçalves <luizhenrique0102@hotmail.com>
2017-11-19 02:06:41 -0200

    Teste
    Correção do pacote

--- src/casadamarmita/CasaDaMarmita.java
+++ src/casadamarmita/CasaDaMarmita.java
@@ -1,11 +1,19 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package casadamarmita;
 
-
+/**
+ *
+ * @author Luiz
+ */
 public class CasaDaMarmita {
     
     
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         Pedido pedido = new Pedido();
-    }*/
+    }
+    
 }
