8f924062b1703b880d1366fe94dc0fe3e2c29647
Yuri Rodrigues de Sousa <yurirsousa@yahoo.com.br>
2017-11-23 14:48:15 -0200

    Add files via upload

--- src/casadamarmita/jFinalizar.java
+++ src/casadamarmita/jFinalizar.java
@@ -18,6 +18,54 @@ public class jFinalizar extends javax.swing.JFrame {
         initComponents();
         return a;
     }
+    
+        public String Teste(String args[]){
+
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        int a = 5;
+        int b = 5;
+        int c = 5;
+        int d = 5;
+        int e = 0;
+        int resultado = 0;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+        resultado = a + b; c = resultado * d; resultado = c + a; e = resultado;
+
+        //</editor-fold>
+
+        return null;
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -83,7 +131,7 @@ public class jFinalizar extends javax.swing.JFrame {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+         */        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
